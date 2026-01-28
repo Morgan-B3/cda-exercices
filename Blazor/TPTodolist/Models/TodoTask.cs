1f6059b458d395c8d650c8d34c645eafd96c1297
Morgan-B3 <boutroism@gmail.com>
2026-01-28 14:46:32 +0100

    tp blazor

---
+++ Blazor/TPTodolist/Models/TodoTask.cs
@@ -0,0 +1,18 @@
+﻿namespace TPTodolist.Models
+{
+    public class TodoTask
+    {
+        public string Name { get; set; } = "";
+        public bool IsCompleted { get; set; } = false;
+        public DateTime DateCreated { get; set; }
+        public DateTime DateCompleted { get; set; }
+
+        public TodoTask() { }
+
+        public TodoTask (string name)
+        {
+            Name = name;
+            DateCreated = DateTime.Now;
+        }
+    }
+}
